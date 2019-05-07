f7b45cf71dc56ef120250e401e21f2b0df75bbb5
Heliovic <1005031096@qq.com>
2019-05-07 23:14:14 +0800

    SNLCompiler: Add LogUtil

---
+++ SNLCompiler/LogUtil.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+#include "LogUtil.h"
+
+
+LogUtil::LogUtil()
+{
+}
+
+
+LogUtil::~LogUtil()
+{
+}
---
+++ SNLCompiler/LogUtil.h
@@ -0,0 +1,11 @@
+/**
+	Log 调试输出工具
+*/
+#pragma once
+class LogUtil
+{
+public:
+	LogUtil();
+	~LogUtil();
+};
+
